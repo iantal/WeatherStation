f23939f1f0b1ed4b20075de00a583324fc17012e
ioanantal <antal.micky@yahoo.com>
2017-07-21 21:22:20 +0300

    added script

---
+++ proj/src/startall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /root/Documents/GIT/WeatherStation/proj/
+source bin/activate
+cd src
+
+echo `pwd`
+
+service rabbitmq-server start
+python manage.py runserver &
+celery -A proj worker -l info -S django &
+celery -A proj beat -l info -S django &
+flower -A proj --port=5555 &
+
